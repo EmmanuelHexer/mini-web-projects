9c9fc5f00e6c9d10a9e3c044f5894573b433a5a0
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-05-14 20:26:14 +0000

    all functionality done

--- Temperature Conversion/script.js
+++ Temperature Conversion/script.js
@@ -7,10 +7,12 @@ function convert() {
   if (toFahrenheit.checked) {
     const celsius = textBox.value;
     const fahrenheit = (celsius * 9) / 5 + 32;
-    result.textContent = fahrenheit;
+    result.textContent = fahrenheit.toFixed(1) + "°F";
   } else if (toCelsius.checked) {
     const fahrenheit = textBox.value;
     const celsius = ((fahrenheit - 32) * 5) / 9;
-    result.textContent = celsius;
+    result.textContent = celsius.toFixed(1) + "°C";
+  } else {
+    result.textContent = "Select a unit";
   }
 }
