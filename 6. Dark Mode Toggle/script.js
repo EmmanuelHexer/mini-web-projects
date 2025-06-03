a65b77a1cfc44fec81a9021bac08d35f8acb1e44
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-06-03 08:02:42 +0000

    Dark mode toggle project

---
+++ 6. Dark Mode Toggle/script.js
@@ -0,0 +1,23 @@
+const inputEl = document.getElementById("dark-mode");
+const bodyEl = document.body;
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+  } else {
+    bodyEl.style.background = "white";
+  }
+}
+
+updateBody();
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
