ebcdab8718b17e8a21926347543bad4cb42b2fe5
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-06-10 03:08:39 +0000

    Animated Search Bar Project

--- 13. Animated Search Bar/script.js
+++ 13. Animated Search Bar/script.js
@@ -1,8 +1,6 @@
-const searchBarContainerEl = document.querySelector(".search-bar-container");
 const magnifierEl = document.querySelector(".magnifier");
+const searchBarContainerEl = document.querySelector(".search-bar-container");
 
 magnifierEl.addEventListener("click", () => {
   searchBarContainerEl.classList.toggle("active");
 });
-
-console.log(magnifierEl, searchBarContainerEl);
